7620cfd175d048623278e28e39ec17a0e0b814cf
ossreleasefeed <sneethling@mozilla.com>
2013-03-13 20:23:48 +0200

    small fix to prevent escaping of HTML tags

--- media/js/plugincheck/tmpl/plugincheck.ui.tmpl.js
+++ media/js/plugincheck/tmpl/plugincheck.ui.tmpl.js
@@ -2,7 +2,7 @@ var vulnerablePluginsTmpl = '{{#vulnerablePlugins}} <tr><td>' +
                         '<section>' +
                         '<img class="icon" width="60" height="60" src="/media/img/plugincheck/app-icons/{{icon}}" alt="Plugin Icon" />' +
                         '<h4 class="name">{{plugin_name}}</h4>' +
-                        '<p class="plugin-detail">{{plugin_detail}}</p>' +
+                        '<p class="plugin-detail">{{{ plugin_detail }}}</p>' +
                         '</section></td>' +
                         '<td class="status">{{plugin_status}}</td>' +
                         '<td class="action"><a href="{{ url }}" class="button button-negative"><span>Update Now</span></a></td>' +
@@ -11,7 +11,7 @@ var vulnerablePluginsTmpl = '{{#vulnerablePlugins}} <tr><td>' +
                         '<section>' +
                         '<img class="icon" width="60" height="60" src="/media/img/plugincheck/app-icons/{{icon}}" alt="Plugin Icon" />' +
                         '<h4 class="name">{{plugin_name}}</h4>' +
-                        '<p class="plugin-detail">{{plugin_detail}}</p>' +
+                        '<p class="plugin-detail">{{{ plugin_detail }}}</p>' +
                         '</section></td>' +
                         '<td class="status">{{plugin_status}}</td>' +
                         '<td class="action"><a href="{{ url }}" class="button button-negative"><span>Update Now</span></a></td>' +
@@ -20,7 +20,7 @@ var vulnerablePluginsTmpl = '{{#vulnerablePlugins}} <tr><td>' +
                         '<section>' +
                         '<img class="icon" width="60" height="60" src="/media/img/plugincheck/app-icons/{{icon}}" alt="Plugin Icon" />' +
                         '<h4 class="name">{{plugin_name}}</h4>' +
-                        '<p class="plugin-detail">{{plugin_detail}}</p>' +
+                        '<p class="plugin-detail">{{{ plugin_detail }}}</p>' +
                         '</section></td>' +
                         '<td class="status">{{plugin_status}}</td>' +
                         '<td class="action"><a href="{{ url }}" class="button research"><span>Research</span></a></td>' +
@@ -29,7 +29,7 @@ var vulnerablePluginsTmpl = '{{#vulnerablePlugins}} <tr><td>' +
                         '<section>' +
                         '<img class="icon" width="60" height="60" src="/media/img/plugincheck/app-icons/{{icon}}" alt="Plugin Icon" />' +
                         '<h4 class="name">{{plugin_name}}</h4>' +
-                        '<p class="plugin-detail">{{plugin_detail}}</p>' +
+                        '<p class="plugin-detail">{{{ plugin_detail }}}</p>' +
                         '</section></td>' +
                         '<td class="status">{{plugin_status}}</td>' +
                         '<td class="action"><a href="{{ url }}" class="button insensitive"><span>Up to Date</span></a></td>' +
